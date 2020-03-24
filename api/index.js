baaeb35faa47e870303520bf9088d7a5f32547ec
kiralybalint <kiraly.balint@simonyi.bme.hu>
2020-03-24 10:07:56 +0100

    Fix CORS

--- api/index.js
+++ api/index.js
@@ -28,6 +28,7 @@ const TWENTY_FOUR_HOURS = 86400000
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   const { query: { url } } = parse(req.url, true)
   if (!url) return send(res, 401, { message: 'Please supply an URL to be scraped in the url query parameter.' })
